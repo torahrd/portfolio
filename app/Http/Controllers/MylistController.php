7e482d458b13b9b90bc49c29d7c99575a00db2c7
torahrd <hrd_trnsk@icloud.com>
2025-05-13 14:27:45 +0000

    DBmigrate

---
+++ app/Http/Controllers/MylistController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class MylistController extends Controller
+{
+    //
+}
---
+++ database/migrations/2025_05_13_122132_create_posts_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained();
+            $table->foreignId('shop_id')->constrained();
+            $table->integer('review');
+            $table->datetime('visit_time')->nullable();
+            $table->integer('budget')->nullable();
+            $table->string('repeat_munu', 200)->nullable();
+            $table->string('intarest_menu', 200)->nullable();
+            $table->string('reference_link', 200)->nullable();
+            $table->string('memo', 500)->nullable();
+            $table->integer('visit_status')->nullable();
+            $table->integer('private_status')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('posts');
+    }
+};
