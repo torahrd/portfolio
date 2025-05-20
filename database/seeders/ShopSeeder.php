764893c20c4583b6ad81d66891b71bc2e3d6bd3e
torahrd <hrd_trnsk@icloud.com>
2025-05-20 12:27:41 +0000

    seeder complete

--- database/seeders/ShopSeeder.php
+++ database/seeders/ShopSeeder.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 use function Illuminate\Support\fake;
 use Illuminate\Support\Carbon;
+use Faker\Factory as Faker;
 
 class ShopSeeder extends Seeder
 {
@@ -15,9 +16,11 @@ class ShopSeeder extends Seeder
      */
     public function run(): void
     {
+        $faker = Faker::create();
+
         DB::table('shops')->insert([
-            'shop' => fake()->company(),
-            'address' => fake()->address(),
+            'name' => $faker->company(),
+            'address' => $faker->address(),
             'created_at' => now(),
             'updated_at' => now(),
         ]);
