87054ac1e3383e8e47a39afc6b928838da25f5f4
torahrd <hrd_trnsk@icloud.com>
2025-06-23 09:58:14 +0000

    refactor: コメントセクションのAlpine.jsロジックを外部JS化

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,6 +4,10 @@ import Alpine from "alpinejs";
 
 window.Alpine = Alpine;
 
+// コメントセクション用Alpine関数をグローバル登録
+import { commentSection } from "./components/comment-section.js";
+window.commentSection = commentSection;
+
 Alpine.start();
 
 // Import post-reply functionality
